c80d9c2ab97a14a4f3648abfb3a2bc4a0059fc75
jdaveyancloudlinux <86103906+jdaveyancloudlinux@users.noreply.github.com>
2021-08-02 14:43:47 +0400

    Update config.js
    Adding viewport meta tag for SEO.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,6 +54,7 @@ module.exports = {
   base: "/",
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
   ],
 
   locales: {
