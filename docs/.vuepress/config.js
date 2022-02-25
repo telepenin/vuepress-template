aa26ea4860e2b826eee553afb0a04512a1d32522
jdaveyancloudlinux <86103906+jdaveyancloudlinux@users.noreply.github.com>
2022-02-25 15:58:27 +0400

    setting referrer meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -55,6 +55,13 @@ module.exports = {
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    [
+      "meta",
+      {
+        name: "referrer",
+        content: "no-referrer-when-downgrade"
+      }
+    ],
   ],
 
   locales: {
