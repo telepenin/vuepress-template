1081abfa004f7b10c7103c3214ffa94616dd6479
Inessa <45294258+iatmachian@users.noreply.github.com>
2022-04-21 13:32:31 +0400

    TOC fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -173,10 +173,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               "/user_interface/",
               "/ids_integration/",
               "/features/",
-              "/config_file_description/",
               "/whmcs_plugin/",
               "/terminology/",
               "/localization/",
+              "/config_file_description/",
               "/faq_and_known_issues/",
               "/uninstall/"
             ]
@@ -232,10 +232,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               "/ru/user_interface/",
               "/ru/ids_integration/",
               "/ru/features/",
-              "/ru/config_file_description/",
               "/ru/whmcs_plugin/",
               "/ru/terminology/",
               "/ru/localization/",
+              "/ru/config_file_description/",
               "/ru/faq_and_known_issues/",
               "/ru/uninstall/"
             ]
