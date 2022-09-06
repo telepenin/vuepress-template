dcd22a06eead27551cd63b34167b7fb694cdef49
jdaveyancloudlinux <86103906+jdaveyancloudlinux@users.noreply.github.com>
2022-09-06 14:30:01 +0400

    csp meta tag

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,6 +63,13 @@ module.exports = {
       }
     ],
     [
+      "meta",
+      {
+        "http-equiv": "Content-Security-Policy",
+        content: "script-src 'self' 'unsafe-inline' 'unsafe-inline' 'unsafe-eval' *.survicate.com *.googletagmanager.com *.google-analytics.com *.licdn.com *.hotjar.com *.twitter.com *.facebook.net *.kernelcare.info *.twitter.com *.ads-twitter.com;"
+      }
+    ],
+    [
       "script",
       {},
       `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
